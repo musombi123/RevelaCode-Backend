6ca0171d48c58db46e2f0082c7d547bf076181e5
Musombi William <musombiwilliam769@gmail.com>
2025-06-21 19:07:58 +0300

    Added mobile-friendly frontend inside cd/revelacode-frontend

--- revelacode-frontend/main.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ProphecyDashboard from './ProphecyDashboard';
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<ProphecyDashboard />);
