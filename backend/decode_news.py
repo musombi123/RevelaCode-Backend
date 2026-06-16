535bf2494e1f0d8760eeb5c5c9745b96bd9e0069
Musombi William <musombiwilliam769@gmail.com>
2026-06-16 21:03:17 +0300

    Added BASE_DIR variable to decode_news.py for better path handling.

--- backend/decode_news.py
+++ backend/decode_news.py
@@ -5,7 +5,9 @@ import os
 # BASE DIRECTORY (backend/)
 # ======================================================
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
 
 # ======================================================
 # PATHS (ALL INSIDE /backend)
